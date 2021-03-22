8770242167399fbed0cd6449d39b65eaf1f62fe4
kroim <wonderfulplayer@outlook.com>
2021-03-22 09:09:58 +0800

    Complete algorithm

--- algorithm.py
+++ algorithm.py
@@ -54,34 +54,99 @@ class Graph:
 
 
 class Node:
-    def __init__(self, parent=None, pos=None):
+    def __init__(self, parent=None, node=None, pos=None):
         self.parent = parent
+        self.node = node
         self.pos = pos
         self.g = 0
         self.h = 0
         self.f = 0
 
-    def _equals(self, item):
-        return self.pos == item.pos
+    def __eq__(self, other):
+        return self.pos == other.pos
 
 
 class Algorithm:
-    def __init__(self, graph, start_node, end_node):
-        self.graph = graph
-        self.start_node = start_node
-        self.end_node = end_node
+    def __init__(self, G):
+        self.G = G
+        self.graph = G.graph
+        self.start_node = G.start_node
+        self.end_node = G.end_node
         self.search()
 
-    def return_path(self):
-        pass
+    def return_path(self, item_node):
+        path = []
+        current = item_node
+        while current is not None:
+            path.append(current)
+            current = current.parent
+        path = path[::-1]
+        for p_item in path:
+            print(self.graph.nodes[p_item.node])
+
+    def get_children(self, item_node):
+        res_nodes = []
+        connected_edges = self.graph.edges(item_node.node)
+        for edge in connected_edges:
+            if edge[0] < item_node.node or edge[1] < item_node.node:
+                continue
+            if edge[0] > item_node.node:
+                res_nodes.append(Node(item_node, edge[0], self.graph.nodes[edge[0]]['pos']))
+            elif edge[1] > item_node.node:
+                res_nodes.append(Node(item_node, edge[1], self.graph.nodes[edge[1]]['pos']))
+        return res_nodes
 
     def search(self):
-        graph = self.graph
-        print(graph.nodes[self.start_node])
-        print(graph.nodes[self.end_node])
+        start_node = Node(None, self.start_node, self.graph.nodes[self.start_node]['pos'])
+        start_node.g = start_node.h = start_node.f = 0
+        end_node = Node(None, self.start_node, self.graph.nodes[self.end_node]['pos'])
+        end_node.g = end_node.h = end_node.f = 0
+        open_list = []
+        close_list = []
+        open_list.append(start_node)
+        outer_iterations = 0
+        max_iterations = 1000
+        # loop until you get the target
+        while len(open_list) > 0:
+            # Every time any node is referred from open_list
+            outer_iterations += 1
+            # Get the current node
+            current_node = open_list[0]
+            current_index = 0
+            for index, item in enumerate(open_list):
+                if item.f < current_node.f:
+                    current_node = item
+                    current_index = index
+            # if path is greater than max_iterations, stop search
+            if outer_iterations > max_iterations:
+                print("Too many iterations for given start, end")
+                return self.return_path(current_node)
+            # Pop current node out off yet_to_visit list, add to visited list
+            open_list.pop(current_index)
+            close_list.append(current_node)
+            # test if goal is reached or not, if yes then return the path
+            if current_node == end_node:
+                return self.return_path(current_node)
+            children = self.get_children(current_node)
+
+            # Loop through children
+            for child in children:
+                # Child is on the visited list (search entire visited list)
+                if len([close_item for close_item in close_list if close_item == child]) > 0:
+                    continue
+
+                # Create the f, g, and h values
+                child.g = current_node.g + self.G.weight
+                # select one child from eucledian distance
+                child.h = (((child.pos[0] - end_node.pos[0]) ** 2) + ((child.pos[1] - end_node.pos[1]) ** 2))
+                child.f = child.g + child.h
+                # Child is already in the open_list and g cost is already lower
+                if len([i for i in open_list if child == i and child.g > i.g]) > 0:
+                    continue
+                open_list.append(child)
 
 
 if __name__ == '__main__':
     M = Maze('input1.txt')
     G = Graph(M.data, '3', '2')
-    A = Algorithm(G.graph, G.start_node, G.end_node)
+    A = Algorithm(G)
