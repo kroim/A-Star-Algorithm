0413f81176abe8c0c8c512f916818c2cef1bd37e
kroim <wonderfulplayer@outlook.com>
2021-03-29 08:17:28 +0800

    Drawing for vizualization

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1, 2, 3, 4, 5]
+y = [1, 2, 3, 4, 5]
+
+fig, ax = plt.subplots(1, 1)
+for i in range(len(x)):
+    if i > 0:
+        plt.plot([x[i-1], x[i]], [y[i-1], y[i]], color='#000')
+for j in range(len(x)):
+    plt.scatter(x[j], y[j], s=100, edgecolors='#000', facecolor='#000')
+fig.show()
 
